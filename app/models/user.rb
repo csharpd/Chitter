8d941a537bdead23f99eb488683c6882fb1b1220
csharpd <chloedonegan@gmail.com>
2014-08-02 17:30:52 +0100

    added a Rakefile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'bcrypt'
+
 class User
 
   include DataMapper::Resource
@@ -6,8 +8,12 @@ class User
   property :name, String
   property :username, String
   property :email, Text
-  property :password, String
+  property :password_digest, Text
 
   # has n, :peeps, :through => Resource
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
 
 end
--- app/server.rb
+++ app/server.rb
@@ -1,20 +1,21 @@
-env = ENV["RACK_ENV"] || "development"
-require 'data_mapper'
 require 'sinatra'
+require 'data_mapper'
+require_relative 'data_mapper_setup'
 
 require './app/controllers/application'
 require './app/controllers/peeps'
 require './app/controllers/users'
 
-
-DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
-
 require './app/models/peep'
 require './app/models/user'
 
+env = ENV["RACK_ENV"] || "development"
+
+DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
+
 DataMapper.finalize
 
-DataMapper.auto_upgrade!
+
 
 
 
