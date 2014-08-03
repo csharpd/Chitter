8d326c276d8a75eec44843c63d1fa5c80df5484e
csharpd <chloedonegan@gmail.com>
2014-08-03 16:15:30 +0100

    Cleaned up server.rb

--- app/server.rb
+++ app/server.rb
@@ -7,14 +7,10 @@ require_relative 'controllers/application'
 require_relative 'controllers/peeps'
 require_relative 'controllers/users'
 require_relative 'controllers/sessions'
+require_relative 'helpers/application'
 
 
 enable :sessions
 set :session_secret, 'super secret'
 use Rack::Flash
 
-helpers do
-  def current_user
-    @current_user ||=User.get(session[:user_id]) if session[:user_id]
-  end
-end
