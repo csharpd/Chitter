3d3b79d695b0c3ae38fa42b52679caa3174128f4
csharpd <chloedonegan@gmail.com>
2014-08-04 17:04:06 +0100

    Peeps are displayed with a date

--- spec/features/listing_all_peeps_spec.rb
+++ spec/features/listing_all_peeps_spec.rb
@@ -6,7 +6,7 @@ feature "User browses the list of peeps" do
     User.create(:name => "Chloe", :username => "CsharpD", :email => "chloe@test.com",
       :password => 'frogs',
       )
-    Peep.create(:content => "A little peep!", :created_at => "2014-08-03 18:40:51")
+    Peep.create(:content => "A little peep!", :created_at => "2014-08-03 18:40:51", :user_id => 1)
   }
 
   scenario "when opening the home page" do
@@ -15,8 +15,8 @@ feature "User browses the list of peeps" do
     sign_in('chloe@test.com','frogs')
     expect(page).to have_content("Welcome, chloe@test.com")
     click_button "Peep!"
-    # expect(page).to have_content("A little peep!")
-    # expect(page).to have_content("2014-08-03T18:40:51+01:00")
+    expect(page).to have_content("A little peep!")
+    expect(page).to have_content("2014-08-03T18:40:51+01:00")
     # expect(page).to have_content("Chloe")
   end
 
