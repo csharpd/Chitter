773e8e73b7c3d2b9778f2d9c5ac8a2203f21c97e
csharpd <chloedonegan@gmail.com>
2014-08-05 18:26:11 +0100

    Fixed error: Should be user_id = user.id

--- spec/features/listing_all_peeps_spec.rb
+++ spec/features/listing_all_peeps_spec.rb
@@ -3,19 +3,14 @@ require 'spec_helper'
 feature "User browses the list of peeps" do
 
   before(:each) {
-    User.create(:name => "Chloe", :username => "CsharpD", :email => "chloe@test.com",
+    user = User.create(:name => "Chloe", :username => "CsharpD", :email => "chloe@test.com",
       :password => 'frogs',
       )
-    Peep.create(:content => "A little peep!", :created_at => "2014-08-03 18:40:51", :user_id => 1)
-
-    visit '/'
-    click_link "Sign in"
-    sign_in('chloe@test.com','frogs')
-    expect(page).to have_content("Welcome, chloe@test.com")
-    click_button "Peep!"
+    Peep.create(:content => "A little peep!", :created_at => "2014-08-03 18:40:51", :user_id => user.id)
   }
 
-  scenario "when opening the home page" do
+  it "when opening the home page" do
+    visit '/'
     expect(page).to have_content("A little peep!")
     expect(page).to have_content("08/03/14 at 06:40PM")
     expect(page).to have_content("Chloe")
