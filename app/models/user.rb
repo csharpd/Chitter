cd6ae4f98edabd7dab4aa516d4f6d49d970b241a
csharpd <chloedonegan@gmail.com>
2014-08-05 20:46:05 +0100

    Fixed peeps

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User
   property :email, String, :unique => true, :message => "This email is already taken"
   property :password_digest, Text
 
-  has n, :peeps
+  has n, :peeps, :through => Resource
 
   def password=(password)
     self.password_digest = BCrypt::Password.create(password)
---
+++ spec/features/replying_to_peeps.rb
@@ -0,0 +1,9 @@
+# require 'spec helper'
+
+# feature 'a signed in user' do
+#   scenario 'can reply to peeps' do
+#     sign_in('test@test.com','test')
+#     visit '/'
+#     click_button 'Reply'
+#   end
+# end
