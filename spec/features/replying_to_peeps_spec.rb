5a667ca7097f12d8881e88c186337261fff5a851
csharpd <chloedonegan@gmail.com>
2014-08-09 11:48:41 +0100

    Changed background

--- spec/features/replying_to_peeps_spec.rb
+++ spec/features/replying_to_peeps_spec.rb
@@ -4,19 +4,12 @@ require_relative 'helpers/session'
 
 include SessionHelpers
 
-feature "Reply" do
+describe Reply do
+  before(:each){ @user = User.create(name: "Oli", email: "oli@gmail.com", username: "olio", password: "eggs", password_confirmation: "eggs") }
+  before(:each){ @peep = Reply.create(message: "Q: What do you call a fake noodle? A: An Impasta", user_id: @user.id) }
 
-  before(:each) { @user = User.create(name: "Oli", email: "oli@gmail.com", username: "olio", password: "eggs", password_confirmation: "eggs")}
-
-  before(:each) { @peep = Peep.create(content: "Q: What do you call a fake noodle? A: An Impasta", user_id: @user_id)}
-
-  scenario 'can be created' do
-    # visit '/'
-    # sign_in('oli@gmail.com','eggs')
-    # fill_in 'reply', with: "Lolz - so funny!"
-    # click_on 'Reply'
-    Reply.create(message: "Lolz - so funny!", peep_id: @peep.id, user_id: @user_id)
+  it 'can be created' do
+    Reply.create(message: "Lolz - so funny!", peep_id: @peep.id, user_id: @user.id)
     expect(Reply.count).to eq 1
-    expect(page).to have_content("Lolz - so funny!")
   end
 end
